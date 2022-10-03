a18a5ff8dcf43f2da7d5c1d40e39b0aa082fdc4f
Kenneth Chow <kenneth@skylight.digital>
2022-10-03 16:38:33 -0700

    added tailwind index

--- pages/index.js
+++ pages/index.js
@@ -7,33 +7,34 @@ import styles from '../styles/Home.module.scss'
 export default function Home() {
   return (
     <div className="home-container">
-      <div className={`${styles.containerBackgroundMedium} ${styles.dashBorder} border-top-0`}>
-        <div className={`container py-5`}>
-          <div className='row'>
-            <div className={`col ${styles.homeText}`}>
-              <h1 className={`text-3xl font-bold ${styles.primaryText} ${styles.headerText}`}>
+      <div className={`bg-blue-100 ${styles.dashBorder} border-top-0`}>
+        <div className='container py-40 mx-20'>
+          <div className='flex flex-row'>
+            <div className='basis-1/2'>
+              <h1 className={`text-6xl mb-10 font-bold ${styles.primaryText} ${styles.headerText}`}>
                 Reponse-ready public health data systems
               </h1>
-              <div className={styles.secondaryText}>
+              <div className='max-w-lg text-blue-600'>
                 Public health data is often messy, heterogeneous, and siloed, and epidemiologists lack the tools and methods to efficiently turn it into meaningful intelligence that can drive timely public health action.
-
+                <br />
+                <br />
                 The Public Health Data Infrastructure (PHDI) project is part of the Pandemic-Ready Interoperability Modernization Effort (PRIME), a multi-year collaboration between CDC and the U.S. Digital Service (USDS) to strengthen data quality and information technology systems in pubilc health departments. This effort seeks to solve these challenges by empowering states, territories, localities, tribes (STLTs), and other public health actors with better, faster, and more accurate data to inform community well-being.
               </div>
             </div>
-            <div className="col d-none d-lg-block">
+            <div className="basis-1/2">
               <div>hello</div>
             </div>
           </div>
         </div>
       </div>
-      <div className={` border-top-0 ${styles.containerBackgroundLight} ${styles.dashBorder} border-top-0`}>
-        <Container className='py-5'>
-          <Row>
-            <Col className="d-none d-lg-block">
-              <div className={`${styles.placeholderMiddle}`}>hello</div>
-            </Col>
-            <Col className={`${styles.homeText} ${styles.containerBackgroundDark} py-5 px-4`}>
-              <h2 className={`text-3xl font-bold ${styles.primaryText} ${styles.headerText}`}>
+      <div className={`border-top-0 bg-blue-50 ${styles.dashBorder} border-top-0`}>
+        <div className='container py-5 mx-20'>
+          <div className='flex flex-row'>
+            <div className="basis-1/2">
+              <div>hello</div>
+            </div>
+            <div className={`basis-1/2 ${styles.homeText} ${styles.containerBackgroundDark} py-12 px-8`}>
+              <h2 className={`text-3xl font-bold mb-5 text-blue-700`}>
                 Introduction to Building Blocks and data modernization
               </h2>
               <div className={styles.secondaryText}>
@@ -43,43 +44,47 @@ export default function Home() {
                 <br /><br />
                 Some Building Blocks offer relatively simple functionality, like standardizing patient names, while others perform more complex tasks, including geocoding and standardizing addresses. Importantly, Building Blocks have been carefully designed with common inputs and outputs making them easily composable across STLTs.
                 <br /><br />
-                Find out more about Building Blocks </div>
-            </Col>
-          </Row>
-        </Container>
+                <div className='font-semibold'>
+                  Find out more about &nbsp;
+                  <a className='underline' href="/">Building Blocks
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={`${styles.containerBackgroundLight} ${styles.dashBorder} border-top-0}`}>
-        <Container className={`py-5 text-primary`}>
-          <Row>
-            <h1 className='row justify-content-center mb-3'>Our pilot partners</h1>
-            <div className='row justify-content-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur commodo habitasse ultrices.</div>
-          </Row>
-          <Row className='justify-content-center my-4'>
-            <Col className={`me-4 ${styles.containerBackgroundDark}`}>
-              <div className='py-4 text-center'>
+      <div className={`bg-blue-50 ${styles.dashBorder} border-top-0} px-20`}>
+        <div className={`container py-12 text-blue-700`}>
+          <h1 className='mb-3 text-3xl font-extrabold text-center'>Our pilot partners</h1>
+          <div className='text-center mb-12'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur commodo habitasse ultrices.</div>
+          <div className='flex flex-row justify-center'>
+            <div className={`mr-8 ${styles.containerBackgroundDark}`}>
+              <div className='p-6 text-center'>
                 <div><b>Heading</b></div>
                 <div className='my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
                 <div>Lorem ipsum</div>
               </div>
-            </Col>
-            <Col className={styles.containerBackgroundDark}>
-              <div className='py-4 text-center'>
+            </div>
+            <div className={styles.containerBackgroundDark}>
+              <div className='p-6 text-center'>
                 <div><b>Heading</b></div>
                 <div className='my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
                 <div>Lorem ipsum</div>
               </div>
-            </Col>
-          </Row>
-        </Container>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={`${styles.containerBackgroundMedium} ${styles.dashBorder} border-top-0}`}>
-        <Container className={`py-5 text-primary ${styles.mw55}`}>
-          <Row>
-            <h2 className='row justify-content-center'>An invitation to participate</h2>
-            <div className='row justify-content-center'>We are currently gathering input from public health agencies and partners to help us define and prioritize future Building Blocks that can help efficiently and effectively collect, use, analyze, and share public health data.  </div>
-            <Button className='mt-3'>Get Involved</Button>
-          </Row>
-        </Container>
+      <div className={`py-28 px-20 bg-blue-100 ${styles.dashBorder} border-top-0}`}>
+        <div className={`container text-blue-700 text-center`}>
+          <h2 className='text-center font-extrabold text-3xl mb-4'>An invitation to participate</h2>
+          <div className='flex justify-center'>
+            <div className='max-w-3xl'>We are currently gathering input from public health agencies and partners to help us define and prioritize future Building Blocks that can help efficiently and effectively collect, use, analyze, and share public health data.  </div>
+          </div>
+
+          <Button className='mt-3'>Get Involved</Button>
+        </div>
       </div>
     </div >
   )
--- pages/partner-with-us.js
+++ pages/partner-with-us.js
@@ -4,24 +4,22 @@ import styles from '../styles/Home.module.scss'
 export default function GetInvolved() {
     return (
         <div>
-            <div className={`${styles.containerBackgroundLight} py-5`}>
-                <Container className="">
-                    <Row className="my-5">
-                        <h1 className={`${styles.centerText} ${styles.primaryText} ${styles.headerText}`}>Partner with us</h1>
-                        <div fluid className={`${styles.centerText} ${styles.secondaryText} ${styles.partnerDescription} mx-auto`}>
+            <div className='bg-blue-50 py-20'>
+                <div className="container max-w-full">
+                    <div className='mb-10'>
+                        <h1 className='text-center text-blue-700 font-extrabold text-6xl mb-4'>Partner with us</h1>
+                        <div fluid className='mx-auto w-7/12 text-center text-blue-700'>
                             Public health agencies have a variety of opportunities to contribute to the development of Building Blocks. Below we've outlined how jurisdictions can get involved at various levels of partnership, including our beta program, focus groups, and Community of Practice (CoP).
                             <br />
                             <br />
                             This is also an opportunity extended to any partner who would like their feedback incorporated into future Building Blocks work, but who may not be immediately available to participate in a pilot.
                         </div>
-                    </Row>
-                    <Row>
-                        {/*Need to fix the padding numbers */}
-                        <Col className={`me-4 py-4 px-5 ${styles.containerBackgroundMedium} ${styles.dashRoundedBorder} {
-}`}>
+                    </div>
+                    <div className='flex flex-rows justify-center'>
+                        <div className={`mr-8 py-8 px-20 bg-blue-100 ${styles.dashRoundedBorder} max-w-lg`}>
                             <div className="text-center mb-3">Picture placeholder</div>
-                            <h4 className="text-center text-primary mb-3">Beta program</h4>
-                            <div className="text-primary">
+                            <h4 className="text-center text-blue-700 mb-3 text-2xl">Beta program</h4>
+                            <div className="text-blue-700 text-sm">
                                 Our beta program is an opportunity for STLTs to define and iterate on existing Building Blocks. By participating in this program, STLTs will gain a greater awareness about their data architecture strengths and weaknesses and help improve a part of their data process.
                                 <br /> <br />
                                 We invite you to visit our SDK toolkit where you can explore the full range of modular Building Blocks. If interested in getting involved in our beta program, please reach out to our team.
@@ -29,26 +27,26 @@ export default function GetInvolved() {
                             <div className="text-center mt-4">
                                 <Button>Learn More</Button>
                             </div>
-                        </Col>
-                        <Col className={`py-4 px-5 ${styles.containerBackgroundMedium} ${styles.dashRoundedBackground}`}>
+                        </div>
+                        <div className={`py-8 px-20 bg-blue-100 ${styles.dashRoundedBorder} max-w-lg`}>
                             <div className="text-center mb-3">Picture placeholder</div>
-                            <h4 className="text-center text-primary mb-3">Focus Groups</h4>
-                            <div className="text-primary">
+                            <h4 className="text-center text-blue-700 mb-3 text-2xl">Focus Groups</h4>
+                            <div className="text-blue-700 text-sm">
                                 We are conducting a series of focus groups to hear back from public health agency staff who interact with data related to childhood lead poisoning, Legionnaires’ disease, listeria, and HIV surveillance at any point along the data lifecycle (e.g., collection, ingestion, management, analysis, visualization).
                                 <br /> <br />
                                 The purpose of these focus groups is to gather STLT input on how we prioritize and design potential Building Blocks. Each focus group will last for approximately 60 minutes.</div>
                             <div className="text-center mt-4">
                                 <Button>Learn More</Button>
                             </div>
-                        </Col>
-                    </Row>
-                </Container>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={`${styles.containerBackgroundMedium} ${styles.dashBorder}`}>
-                <Container className={`py-5`}>
-                    <Row className={`${styles.communityContainer} mx-auto`}>
-                        <h4 className="text-center mb-3 text-primary">You can also help support the Community of Practice</h4>
-                        <div className="text-center mb-4 text-primary">
+            <div className={`bg-blue-100 ${styles.dashBorder}`}>
+                <div className='container py-10 max-w-full'>
+                    <div className={`max-w-4xl mx-auto`}>
+                        <h4 className="text-center text-blue-700 mb-3 text-2xl font-medium">You can also help support the Community of Practice</h4>
+                        <div className="text-center mb-4 text-blue-700 font-normal font-sm">
                             A CoP partner is a STLT who, in the immediate future, is interested in offering feedback about the design principles and application interface(s) for Building Blocks and, in the near future, is interested in participating to the Building Block ecosystem by contributing code, helping govern the open source community, or offering other services as needed.
                         </div>
                         <div className="text-center width-fit">
@@ -57,8 +55,8 @@ export default function GetInvolved() {
                             </Button>
                         </div>
 
-                    </Row>
-                </Container>
+                    </div>
+                </div>
             </div>
         </div >
     );
