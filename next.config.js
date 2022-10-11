3f327ad63f9838516a4938dfa7603a179d67faba
Kenneth Chow <kenneth@skylight.digital>
2022-10-11 13:59:54 -0700

    added akamai

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: ''
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
