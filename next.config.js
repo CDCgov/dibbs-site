65ff5a9e5e9dda7f9d8e0bf41c4e66f90652ec88
jnygaard <josh.nygaard@gmail.com>
2023-12-26 14:40:08 -0500

    install sharp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
