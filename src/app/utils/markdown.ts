173999ac4501d7de538b128d1a9f4b8bca88c0c5
nickbristow <richard.nick.bristow@gmail.com>
2024-10-10 16:27:01 -0400

    parse markdown content

---
+++ src/app/utils/markdown.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as matter from 'gray-matter';
+
+export async function getMarkdownContent(fileName) {
+  const markdownFilePath = path.join(process.cwd(), 'src/content', fileName);
+  const fileContents = fs.readFileSync(markdownFilePath, 'utf8');
+  const { content } = matter(fileContents, undefined);
+
+  return content
+}
