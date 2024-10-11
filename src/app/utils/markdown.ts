0a4f1d6de4a576244be84a82097b7367d48f301b
nickbristow <richard.nick.bristow@gmail.com>
2024-10-11 15:03:17 -0400

    type

--- src/app/utils/markdown.ts
+++ src/app/utils/markdown.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as matter from 'gray-matter';
 
-export async function getMarkdownContent(fileName) {
+export async function getMarkdownContent(fileName: string) {
   const markdownFilePath = path.join(process.cwd(), 'src/content', fileName);
   const fileContents = fs.readFileSync(markdownFilePath, 'utf8');
   const { content } = matter(fileContents, undefined);
