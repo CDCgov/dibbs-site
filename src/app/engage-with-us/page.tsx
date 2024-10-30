93f1115d5cdd756d262d0e412446f138def7fb04
Jake Wheeler <jake.wheeler@skylight.digital>
2024-10-30 19:52:55 -0400

    add hero to other pages

---
+++ src/app/engage-with-us/page.tsx
@@ -0,0 +1,12 @@
+import Hero from '../components/Hero';
+
+export default function EngageWithUs() {
+  return (
+    <div>
+      <Hero
+        header="Get started with DIBBs products"
+        subheader="Learn how your jurisdiction can start working with the DIBBs team."
+      />
+    </div>
+  );
+}
