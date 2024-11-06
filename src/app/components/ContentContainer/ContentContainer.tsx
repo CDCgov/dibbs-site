de5b522f9d0cf5edb2af6053679d7e82fdc27f16
Jake Wheeler <jake.wheeler@skylight.digital>
2024-11-06 12:52:53 -0500

    allow style override for content container

--- src/app/components/ContentContainer/ContentContainer.tsx
+++ src/app/components/ContentContainer/ContentContainer.tsx
@@ -3,20 +3,19 @@ import classNames from 'classnames';
 interface ContentContainerProps {
   children: React.ReactNode;
   align?: boolean;
+  classes?: string;
 }
 export function ContentContainer({
   children,
   align = false,
+  classes = 'ml-auto mr-auto flex max-w-[87.5rem] flex-col px-10 py-4 sm:px-14 sm:py-20',
 }: ContentContainerProps) {
   return (
     <section>
       <div
-        className={classNames(
-          'ml-auto mr-auto flex max-w-[87.5rem] flex-col px-10 py-4 sm:px-14 sm:py-20',
-          {
-            'md:px-32': !align,
-          },
-        )}
+        className={classNames(classes, {
+          'md:px-32': !align,
+        })}
       >
         {children}
       </div>
