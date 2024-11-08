30105a7a9aeca7f6ae5726a280c72da2acacc04e
Jake Wheeler <jake.wheeler@skylight.digital>
2024-11-08 09:02:20 -0500

    allow linkbutton to take class names from user

--- src/app/components/LinkButton/LinkButton.tsx
+++ src/app/components/LinkButton/LinkButton.tsx
@@ -1,14 +1,17 @@
+import classNames from 'classnames';
 import Link, { LinkProps } from 'next/link';
 
 interface LinkButtonProps extends LinkProps {
   children: React.ReactNode;
   variant: 'primary' | 'secondary';
+  className?: string;
   disabled?: boolean;
 }
 export function LinkButton({
   children,
   variant,
   href,
+  className = '',
   disabled = false,
   ...props
 }: LinkButtonProps) {
@@ -16,7 +19,10 @@ export function LinkButton({
     return (
       <Link
         href={href}
-        className="usa-button hover:bg-red items-center justify-start gap-2.5 rounded border-2 border-[#224a58] bg-white px-5 py-3 hover:border-2"
+        className={classNames(
+          'usa-button hover:bg-red items-center gap-2.5 rounded border-2 border-[#224a58] bg-white px-5 py-3 hover:border-2',
+          className,
+        )}
         {...props}
       >
         <span className="text-center text-base font-bold leading-none text-[#224a58]">
@@ -33,7 +39,10 @@ export function LinkButton({
   return (
     <Link
       href={href}
-      className="usa-button hover:bg-green self-end justify-self-start bg-[#224a58]"
+      className={classNames(
+        'usa-button hover:bg-green bg-[#224a58]',
+        className,
+      )}
       {...props}
     >
       <span className="text-center text-base font-bold text-white">
