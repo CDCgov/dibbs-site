53328113f4d1aaa228564869c2041231535b09c0
Jake Wheeler <jake.wheeler@skylight.digital>
2024-11-13 14:53:45 -0500

    fix favicon 500 error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,11 @@ import type { Metadata } from 'next';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import USABanner from './components/UsaBanner/UsaBanner';
-import { basePath } from './utils/constants';
 import './globals.scss';
 import './custom-styles.css';
 
 export const metadata: Metadata = {
   title: 'DIBBS Site',
-  icons: {
-    icon: `${basePath}/app/favicon.ico`,
-  },
 };
 
 export default function RootLayout({
