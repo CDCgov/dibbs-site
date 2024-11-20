c93cbafab79f95cb0951a8e06c17f0cf7fcb0250
nickbristow <richard.nick.bristow@gmail.com>
2024-11-20 14:53:02 -0500

    Add footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -29,35 +29,47 @@ export default function Footer() {
   ];
 
   return (
-    <footer className="usa-header--basic bg-background-teal">
-      <div className="usa-nav-container flex-vertical-center">
-        <div className="usa-navbar">
-          <div className="usa-logo">
-            <em className="usa-logo__text">
-              <a href="http://cdc.gov" title="<Project title>">
-                <span className={classNames('sr-only', styles.navbarLogoText)}>
-                  CDC US center for disease control and prevention
-                </span>
-                <Image
-                  width={200}
-                  height={40}
-                  alt=""
-                  className="margin-x-0"
-                  src={`${basePath}/images/CDC.svg`}
-                />
-              </a>
-            </em>
+    <>
+      <div
+        className={classNames(
+          styles['footer-phdi'],
+          'usa-header--basic bg-background-teal',
+        )}
+      ></div>
+      <footer
+        className={classNames('usa-header--basic bg-background-dark-blue')}
+      >
+        <div className="usa-nav-container flex-vertical-center">
+          <div className="usa-navbar">
+            <div className="usa-logo">
+              <em className="usa-logo__text">
+                <a href="http://cdc.gov" title="<Project title>">
+                  <span
+                    className={classNames('sr-only', styles.navbarLogoText)}
+                  >
+                    CDC US center for disease control and prevention
+                  </span>
+                  <Image
+                    width={200}
+                    height={40}
+                    alt=""
+                    className="margin-x-0"
+                    src={`${basePath}/images/CDC.svg`}
+                  />
+                </a>
+              </em>
+            </div>
+            <button type="button" className="usa-menu-btn">
+              Menu
+            </button>
           </div>
-          <button type="button" className="usa-menu-btn">
-            Menu
-          </button>
+          <PrimaryNav
+            items={testItemsMenu}
+            mobileExpanded={expanded}
+            onToggleMobileNav={onClick}
+          />
         </div>
-        <PrimaryNav
-          items={testItemsMenu}
-          mobileExpanded={expanded}
-          onToggleMobileNav={onClick}
-        />
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
