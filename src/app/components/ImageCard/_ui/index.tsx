709ee07f826c7e56dbbbaef7c1f579571a780bc7
nickbristow <richard.nick.bristow@gmail.com>
2024-11-27 10:44:56 -0500

    refactor

--- src/app/components/ImageCard/_ui/index.tsx
+++ src/app/components/ImageCard/_ui/index.tsx
@@ -6,11 +6,10 @@ interface ContainerProps {
   imageFirst?: boolean;
 }
 
-function Container({ children, imageFirst = true }: ContainerProps) {
-  let imageFirstClass = 'xl:grid-cols-[1fr_2fr]';
-  if (!imageFirst) {
-    imageFirstClass = 'xl:grid-cols-[2fr_1fr]';
-  }
+export const Container = ({ children, imageFirst = true }: ContainerProps) => {
+  const imageFirstClass = imageFirst
+    ? 'xl:grid-cols-[1fr_2fr]'
+    : 'xl:grid-cols-[2fr_1fr]';
 
   return (
     <div
@@ -23,45 +22,32 @@ function Container({ children, imageFirst = true }: ContainerProps) {
       {children}
     </div>
   );
-}
+};
 
-function ImageContainer({
-  children,
-  imageFirst = true,
-}: {
+interface ChildrenProps {
   children: React.ReactNode;
   imageFirst: boolean;
-}) {
-  return (
-    <div
-      className={classNames(
-        imageFirst ? styles.imageContainer : styles['image-box-reverse'],
-        'image-box h-full w-[30rem] overflow-hidden',
-        imageFirst ? 'order-1' : 'order-1 xl:order-2',
-      )}
-    >
-      {children}
-    </div>
-  );
 }
 
-function TextContainer({
-  children,
-  imageFirst = true,
-}: {
-  children: React.ReactNode;
-  imageFirst: boolean;
-}) {
-  return (
-    <div
-      className={classNames(
-        'text-box order-2 flex flex-col gap-5 pb-10 pt-10',
-        imageFirst ? '' : 'xl:order-1 xl:pl-10',
-      )}
-    >
-      {children}
-    </div>
-  );
-}
+export const ImageContainer = ({ children, imageFirst }: ChildrenProps) => (
+  <div
+    className={classNames(
+      imageFirst ? styles.imageContainer : styles['image-box-reverse'],
+      'image-box h-full w-[30rem] overflow-hidden',
+      imageFirst ? 'order-1' : 'order-1 xl:order-2',
+    )}
+  >
+    {children}
+  </div>
+);
 
-export { Container, ImageContainer, TextContainer };
+export const TextContainer = ({ children, imageFirst }: ChildrenProps) => (
+  <div
+    className={classNames(
+      'text-box order-2 flex flex-col gap-5 pb-10 pt-10',
+      imageFirst ? '' : 'xl:order-1 xl:pl-10',
+    )}
+  >
+    {children}
+  </div>
+);
