e98880a38ca61b13728a110442421989b026c930
Jake Wheeler <jake.wheeler@skylight.digital>
2024-11-27 11:41:24 -0500

    use arrow functions

--- src/app/case-studies/_ui/index.tsx
+++ src/app/case-studies/_ui/index.tsx
@@ -5,27 +5,27 @@ interface ContainerProps {
   children: React.ReactNode;
 }
 
-function PageContainer({ children }: ContainerProps) {
+const PageContainer = ({ children }: ContainerProps) => {
   return (
     <div className="main ml-auto mr-auto flex items-center justify-center pb-20 lg:pl-[7.5rem] lg:pr-[7.5rem]">
       {children}
     </div>
   );
-}
+};
 
-function ContentContainer({ children }: ContainerProps) {
+const ContentContainer = ({ children }: ContainerProps) => {
   return (
     <div className="content grid max-w-[53rem] grid-cols-1 gap-[3.75rem] pl-[3.75rem] pr-[3.75rem] pt-10">
       {children}
     </div>
   );
-}
+};
 
-function SectionContentContainer({ children }: ContainerProps) {
+const SectionContentContainer = ({ children }: ContainerProps) => {
   return <div className="flex flex-col gap-3">{children}</div>;
-}
+};
 
-function ReturnToCaseStudiesLink() {
+const ReturnToCaseStudiesLink = () => {
   return (
     <Link
       className="font-['Public Sans'] text-base font-normal leading-relaxed text-[#3a7d95] underline"
@@ -34,21 +34,21 @@ function ReturnToCaseStudiesLink() {
       Return to all case studies
     </Link>
   );
-}
+};
 
-function Text({ children }: ContainerProps) {
+const Text = ({ children }: ContainerProps) => {
   return <p className="m-0 flex flex-col gap-6 p-0">{children}</p>;
-}
+};
 
-function UnorderedList({ children }: ContainerProps) {
+const UnorderedList = ({ children }: ContainerProps) => {
   return (
     <ul className="list__full-width flex flex-col gap-2 font-semibold">
       {children}
     </ul>
   );
-}
+};
 
-function ReadMore({ href, linkText }: { href: string; linkText: string }) {
+const ReadMore = ({ href, linkText }: { href: string; linkText: string }) => {
   return (
     <>
       <h2>Read more about our work</h2>
@@ -62,7 +62,7 @@ function ReadMore({ href, linkText }: { href: string; linkText: string }) {
       </ExternalLink>
     </>
   );
-}
+};
 
 export {
   ContentContainer,
