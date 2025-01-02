49cd555bf79d0c13348e5b6bfe0cfb62fafa772d
Jake Wheeler <jake.wheeler@skylight.digital>
2025-01-02 15:19:17 -0500

    use swap for fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,11 +3,13 @@ import { Merriweather, Public_Sans } from 'next/font/google';
 export const merriweather = Merriweather({
   subsets: ['latin'],
   weight: ['400', '700'],
+  display: 'swap',
   variable: '--font-merriweather',
 });
 
 export const publicSans = Public_Sans({
   subsets: ['latin'],
   weight: ['400', '600', '700'],
+  display: 'swap',
   variable: '--font-public-sans',
 });
