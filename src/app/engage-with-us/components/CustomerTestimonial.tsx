46e4c5c514575eb27bf390ef572545231c56c231
nickbristow <richard.nick.bristow@gmail.com>
2025-01-08 11:01:20 -0500

    italics text block

--- src/app/engage-with-us/components/CustomerTestimonial.tsx
+++ src/app/engage-with-us/components/CustomerTestimonial.tsx
@@ -8,12 +8,12 @@ export const CustomerTestimonial = () => {
         <p className="font-['Public Sans'] m-0 p-0 font-light uppercase leading-normal text-blue-cool-60">
           Customer Testimonial
         </p>
-        <p className="font-['Public Sans'] m-0 max-w-[28rem] p-0 text-[1rem] font-bold leading-[2rem] text-blue-cool-70 xl:text-[1.38rem]">
+        <p className="font-['Public Sans'] m-0 max-w-[28rem] p-0 text-[1rem] font-bold italic leading-[2rem] text-blue-cool-70 xl:text-[1.38rem]">
           “Our epidemiologists waste 80% of their time cleaning data and can't
           do useful analysis. The end goal of DIBBs infrastructure is to free up
           that 80% of their time to do actual public health work.”
         </p>
-        <p className="m-0 p-0 text-[1rem] font-light leading-normal text-blue-cool-70 lg:text-nowrap xl:min-w-full">
+        <p className="m-0 p-0 text-[1rem] font-light italic leading-normal text-blue-cool-70 lg:text-nowrap xl:min-w-full">
           - Public Health Official, Virginia Department of Health
         </p>
       </div>
