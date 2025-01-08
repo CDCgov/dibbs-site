a1b3f64ca291991616c882a8d22f12cf438ed02b
Jake Wheeler <jake.wheeler@skylight.digital>
2025-01-08 10:05:35 -0500

    update value section button wording

--- src/app/data/home.ts
+++ src/app/data/home.ts
@@ -15,7 +15,7 @@ export const homeContent = {
     title: "Unlock the value of your jurisdiction's data",
     description:
       'DIBBs products help jurisdictions make the most of their data. From improving the usability of electronic case reporting (eCR) data to streamlining data collection from healthcare providers without the need for a direct connection, our products save jurisdictions time and effort for case investigation and analysis.',
-    ctaText: 'Find out more about our products',
+    ctaText: 'Learn about our products',
     ctaHref: '/products',
   },
   jurisdictions: {
