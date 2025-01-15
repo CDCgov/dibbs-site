7f4a83c0bed23fe37366245aceb186c7d3658f8c
Jake Wheeler <jake.wheeler@skylight.digital>
2025-01-15 10:00:59 -0500

    dark blue bg for header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        'dark-blue': '#1D183C',
         success: {
           DEFAULT: '#446443',
         },
