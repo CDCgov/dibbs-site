ce4b4a81d495e1ad24fd43a05cc138ab64433af7
Jake Wheeler <jake.wheeler@skylight.digital>
2025-01-23 13:22:10 -0500

    fix max cta height

--- src/app/components/InvitationCta/InvitationCta.tsx
+++ src/app/components/InvitationCta/InvitationCta.tsx
@@ -34,7 +34,7 @@ export const InvitationCta = () => {
   };
 
   return (
-    <section style={style} className="max-h-[24.25rem] py-[3.75rem]">
+    <section style={style} className="max-h-[16.25rem] py-[3.75rem]">
       <GridContainer>
         <div className="flex flex-col items-center justify-center gap-6 self-stretch">
           <div className="flex flex-col gap-1">
